d2cd3078ef9b7bfdeea6aab18a02d2abb9fde9f0
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2023-10-30 18:20:53 -0700

    Submitted for help/grading

--- django/techproject/src/mainapp/products/views.py
+++ django/techproject/src/mainapp/products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .form import ProductForm
+from .forms import ProductForm
 from .models import Product
 
 
